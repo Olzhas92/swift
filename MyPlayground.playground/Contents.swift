999726760bf371b73c26c5cfe35d770da8d91774
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-03 20:25:00 +0600

    first assignments

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var name = "Olzhas"
+var num1 = 10
 
-print(greeting)
+print("My name is \(name). My number is \(num1)")
+
+var a = 10
+var b = 5
+var x = 7
+var y = 9
+
+var res = (a + 4*b)*(a-3*b)+a*2-5-b
+print("Result is \(res)")
+print("Is result bigger than zero?", res > 0)
