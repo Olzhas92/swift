de90b86a91f306fb1f6d668737c3ccade4984c97
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-05 21:25:54 +0600

    class assignments

--- CW0410-Variables.playground/Contents.swift
+++ CW0410-Variables.playground/Contents.swift
@@ -1,56 +1,10 @@
 import UIKit
 
-let seasons = 4
-let week = 7
-let month = 4
-print("Number of seasons - \(seasons). Days in a week - \(week). Weeks in a month - \(month).")
+var name = "Olzhas"
+var age = 30
+var weight = 78.85
+var isMarried = true
+var lastNameChar = "K"
 
-var myString = "hello world"
-for item in myString {
-    print(item)
-}
-
-print("Number of charaters is \(myString.count)")
-
-print("entire array")
-for index in 1...20 {
-    print(index)
-}
-
-print("even numbers")
-for index in 1...20 {
-    if(index % 2 == 0) {
-        print(index, "\t")
-    }
-}
-
-print("tenth")
-for index in 1...20 {
-    if(index % 10 == 0) {
-        print(index, "\n")
-    }
-}
-
-print("divided by 4")
-for index in 1...20 {
-    if(index % 4 == 0) {
-        print("\(index)")
-    }
-}
-
-var num25 = 25
-var num15 = 15
-var stringEquation = "\n6x + 15y = 39"
-
-var res1 = "\(num25)x - \(num15)y = 85"
-var totalRes = res1 + stringEquation
-print(totalRes)
-
-
-func greeting(person: String) -> String {
-    let greeting = "Hello, " + person
-    return greeting
-}
-
-var res = greeting(person: "Olzhas")
+var res = "My name is \(name), my lastname first letter is \(lastNameChar). I am \(age) y.o. My weight is \(weight). Marital status = \(isMarried)"
 print(res)
