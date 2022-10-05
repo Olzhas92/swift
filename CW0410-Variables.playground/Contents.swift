b809be5684a2fac5388015170370e93d52203a3a
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-05 20:24:59 +0600

    strings and loops

--- CW0410-Variables.playground/Contents.swift
+++ CW0410-Variables.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let seasons = 4
+let week = 7
+let month = 4
+print("Number of seasons - \(seasons). Days in a week - \(week). Weeks in a month - \(month).")
+
+var myString = "hello world"
+for item in myString {
+    print(item)
+}
+
+print("entire array")
+for index in 1...20 {
+    print(index)
+}
+
+print("even numbers")
+for index in 1...20 {
+    if(index % 2 == 0) {
+        print(index, "\t")
+    }
+}
+
+print("tenth")
+for index in 1...20 {
+    if(index % 10 == 0) {
+        print(index, "\n")
+    }
+}
+
+print("divided by 4")
+for index in 1...20 {
+    if(index % 4 == 0) {
+        print("\(index)")
+    }
+}
