32aeb4578fa6eef80f60b4e48abe561078c4fa4f
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-09 21:51:03 +0600

    task 2 completed

--- CW0410-Variables.playground/Contents.swift
+++ CW0410-Variables.playground/Contents.swift
@@ -1,1 +1,12 @@
 import UIKit
+
+// Task 2
+var sphereRadius = 5
+var sphereHeight = 7
+var square: Double
+
+square = Double(4*sphereRadius*sphereRadius)*3.14
+print("Square of the sphere is \(square)")
+
+// Task 3
+
