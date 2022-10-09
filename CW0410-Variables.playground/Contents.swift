5c84e9746bc73cea84b7bdaee9f252c636d7edb1
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-09 21:56:23 +0600

    hw completed

--- CW0410-Variables.playground/Contents.swift
+++ CW0410-Variables.playground/Contents.swift
@@ -1,12 +1,27 @@
 import UIKit
 
 // Task 2
+var cylinderRadius = 5
+var cylinderHeight = 7
+var cylinderSquare: Double
+cylinderSquare = Double(2 * cylinderRadius * (cylinderHeight + cylinderRadius)) * 3.14
+print("Square of the cylinder is \(cylinderSquare)")
+
+
+// Task 3
 var sphereRadius = 5
-var sphereHeight = 7
 var square: Double
 
 square = Double(4*sphereRadius*sphereRadius)*3.14
 print("Square of the sphere is \(square)")
 
-// Task 3
+// Task 4
+var a = 5
+var b = 6
+var c = 7
+var d = 8
+var x = 10
+var result: Int
+result = (a*x+b) * (c*x+d)
+print("Result of the equation is \(result)")
 
