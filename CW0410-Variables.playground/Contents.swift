a9ecc1b9d03c84f7fb910a930a4c45fa519bf9b6
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-09 21:45:51 +0600

    homework as of 05.10

--- CW0410-Variables.playground/Contents.swift
+++ CW0410-Variables.playground/Contents.swift
@@ -1,13 +1,1 @@
 import UIKit
-
-print("*")
-
-var name = "Olzhas"
-var lastName = "Khamzin"
-var age = 30
-var hobbies = ["reading", "hiking"]
-var favouriteColor = "white"
-var favouriteGame = "football"
-var favouriteMovie = "Game Of Thrones"
-
-print("My name is \(lastName) \(name). I am \(age) yeas old. My hobbies are \(hobbies[0]) and \(hobbies[1]). My favourite color is \(favouriteColor), favourite game - \(favouriteGame), and my favoruite TV series is \(favouriteMovie). It was nice to meet you!")
