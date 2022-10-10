e993e5eee05f7ec9d724b785d020515c420613e5
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-10 21:46:26 +0600

    all tasks completed

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,16 +1,23 @@
 import UIKit
 
 // Task 1
-func maxFunc(myArr: [Int]) -> Void {
-    var max = myArr[0]
-    for item in myArr {
-        if(item > max) {
-            max = item
-        }
-    }
-    print("max is \(max)")
+//func maxFunc(myArr: [Int]) -> Void {
+//    var max = myArr[0]
+//    for item in myArr {
+//        if(item > max) {
+//            max = item
+//        }
+//    }
+//    print("max is \(max)")
+//}
+//maxFunc(myArr: [7,8,9])
+
+func ternaryMax(num1: Int, num2: Int, num3: Int) -> Int {
+    print("the biggest number is")
+    return (num1 > num2) ? (num1 > num3 ? num1: num3) : (num2 > num2 ? num2 : num3)
 }
-maxFunc(myArr: [7,8,9])
+
+print(ternaryMax(num1: 7, num2: 8, num3: 15))
 
 
 // Task 2
@@ -39,3 +46,58 @@ func triangleFunc(A: Int, B:Int, C:Int) -> Int {
     return perimeter
 }
 print(triangleFunc(A: 5, B: 6, C: 7))
+
+// Task 4
+func myMarks(myMark: String) -> Void {
+    switch myMark {
+    case "A": print("\(myMark) is 90-100 points")
+    case "B": print("\(myMark) is 75-89 point")
+    case "C": print("\(myMark) is 50-75 points")
+    default:
+        print("there is no such a mark")
+    }
+}
+
+myMarks(myMark: "A")
+
+// Task 4a
+func percentageMarks(myMark: Int) -> String {
+    if (myMark >= 90) {
+        return "The mark is A"
+    }
+    else if(myMark >= 75 && myMark < 90) {
+        return "The mark is B"
+    }
+    else if(myMark >= 50 && myMark < 75) {
+        return "The mark is C"
+    }
+    else {
+        return "There is no such a mark"
+    }
+}
+
+print(percentageMarks(myMark: 90))
+
+// Task 5
+func myElectricityBills(myUnit: Double) -> Double {
+    var res: Double = 0
+    if(myUnit <= 50) {
+        let percents: Double = ((myUnit * 0.50) / 20) * 100
+        res = (myUnit * 0.50) + percents
+    }
+    else if(myUnit > 50 && myUnit < 100) {
+        let percents: Double = ((myUnit * 0.50) / 20) * 100
+        res = (myUnit * 0.75) + percents
+    }
+    else if (myUnit > 100 && myUnit < 250) {
+        let percents: Double = ((myUnit * 0.50) / 20) * 100
+        res = (myUnit * 1.20) + percents
+    }
+    else if (myUnit >= 250) {
+        let percents: Double = ((myUnit * 0.50) / 20) * 100
+        res = (myUnit * 1.50) + percents
+    }
+    return res
+}
+
+print(myElectricityBills(myUnit: 500))
